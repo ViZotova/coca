ab33a9d0757b502fc18f5a39c41ccdb283d033b4
ViZotova <zotovavo@mail.ru>
2024-09-01 20:12:04 +0500

    Added scss

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    root: "./src",
-    server: {
-        port: 3000,
-    },
-    build: {
-        outDir: "../dist",
-    },
+  root: "./src",
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: 'true'
+  },
 });
