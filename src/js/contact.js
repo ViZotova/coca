60c64f62ec7ef3e1aae449a2de3edfc2bc4c4020
ViZotova <zotovavo@mail.ru>
2024-10-01 22:16:14 +0300

    Created contact:hero and added contact partners

---
+++ src/js/contact.js
@@ -0,0 +1,8 @@
+import "../assets/scss/contact.scss";
+
+// components
+import { useTheme } from "./components/theme";
+import { useBurger } from "./components/burger";
+
+useTheme();
+useBurger();
--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
       input: {
         main: "./src/index.html",
         pricing: "./src/pricing.html",
+        contact: "./src/contact.html",
       },
     },
   },
